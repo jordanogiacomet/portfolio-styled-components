0f5ae613693a9093920697ac91bcec623f0582fe
Jordano <jgtomazoni@ucs.br>
2024-07-16 21:34:38 -0300

    WIP - Todos os componentes necessários implementados; Implementar testes e storybooks; Integrar com API do resend;

--- src/components/AnimatedText/index.jsx
+++ src/components/AnimatedText/index.jsx
@@ -3,21 +3,17 @@ import * as Styled from './styles';
 export const AnimatedText = () => {
   return (
     <Styled.Container
-    className="animated-text"
-    sequence={[
-      'Jordano',
-      1000,
-      'Web Developer',
-      1000,
-      'Backend Developer',
-      1000,
-      'Fullstack Developer',
-      1000,
-    ]}
-    wrapper="span"
-    speed={50}
-    repeat={Infinity}
-  />
+      className="animated-text"
+      sequence={[
+        'Jordano',
+        1000,
+        'Web Developer',
+        1000,
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
   );
 };
 
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -11,11 +11,11 @@ export const GlobalStyles = createGlobalStyle`
   }
   body {
     font-size: 1.6rem;
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Montserrat', sans-serif !important;
   }
 
   h1, h2, h3, h4, h5, h6{
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Montserrat', sans-serif !important;
   }
 
   button {
